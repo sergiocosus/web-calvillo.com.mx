5b0067aac7b27eae074ac79b15fb04b52f0d07f9
Sergio Contreras Sustaita <sergio.contreras@triolabs.mx>
2017-08-09 21:42:36 -0500

    Fix import from rxjs package to allow to use --build-optimizer

--- src/client.ts
+++ src/client.ts
@@ -5,6 +5,10 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
 
 /**
  * enable prod mode for production environments
