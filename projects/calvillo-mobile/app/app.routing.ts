9c461b171b9a98752facedc68d7b948f7b138b85
Sergio Contreras Sustaita <sergiocosus@gmail.com>
2018-07-25 14:35:15 -0500

    Added automatic scroll, mark selected picture in list

--- projects/calvillo-mobile/app/app.routing.ts
+++ projects/calvillo-mobile/app/app.routing.ts
@@ -7,7 +7,8 @@ import { ItemDetailComponent } from './item/item-detail.component';
 import { environment } from '~/environment';
 
 const routes: Routes = [
-    {path: '', redirectTo: '/galeria/' + environment.defaultCategoryId, pathMatch: 'full'},
+    {path: '', redirectTo: '/galeria/guayabas-de-el-portn_1/foto/guayabas-y-sus-beneficios', pathMatch: 'full'},
+    //{path: '', redirectTo: '/galeria/' + environment.defaultCategoryId, pathMatch: 'full'},
     {path: 'items', component: ItemsComponent},
     {path: 'item/:id', component: ItemDetailComponent},
     {path: '', loadChildren: './pages/pages.module#PagesModule'},
--- projects/calvillo-mobile/app/pages/picture-gallery-page/picture-gallery-page/picture-gallery-page.component.ts
+++ projects/calvillo-mobile/app/pages/picture-gallery-page/picture-gallery-page/picture-gallery-page.component.ts
@@ -1,82 +1,152 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { finalize } from 'rxjs/operators';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  QueryList,
+  ViewChildren
+} from '@angular/core';
+import { finalize, switchMap } from 'rxjs/operators';
 import {
   Category,
   CategoryService,
   Picture,
   PictureService
 } from '@calvillo/api';
-import {
-  SwipeDirection,
-  SwipeGestureEventData
-} from 'tns-core-modules/ui/gestures';
+import { PageRoute, RouterExtensions } from 'nativescript-angular/router';
+
+import * as _ from 'lodash';
+import { ViewChild } from '@angular/core';
+import { ScrollView } from 'tns-core-modules/ui/scroll-view';
+import { FlexboxLayout } from 'tns-core-modules/ui/layouts/flexbox-layout';
 
 
 @Component({
   selector: 'app-picture-gallery-page',
   moduleId: module.id,
-  templateUrl: './p.html',
+  templateUrl: './picture-gallery-page.component.html',
   styleUrls: ['./picture-gallery-page.component.scss']
 })
 export class PictureGalleryPageComponent implements OnInit {
+  @ViewChild('scrollView') scrollview: ElementRef;
+  @ViewChildren('thumb') thumbsRef: QueryList<ElementRef>;
+
   private categoryLink: string | null;
   private loading: boolean;
   category: Category;
   private pictureLink: string | null;
-  picture1: Picture;
-  picture2: Picture;
+
   index = 0;
   pictureLength: number;
+  private picture: Picture;
+  private oldIndex: number;
 
   constructor(private categoryService: CategoryService,
               private pictureService: PictureService,
-              private route: ActivatedRoute,
+              private pageRoute: PageRoute,
+              private routerExtensions: RouterExtensions
   ) {
-    this.route.paramMap.subscribe(
+    this.pageRoute.activatedRoute.pipe(
+      switchMap(activatedRoute => activatedRoute.paramMap)
+    ).subscribe(
       params => {
-        this.categoryLink = params.get('categoryLink');
-        this.pictureLink = params.get('pictureLink');
+        if (this.categoryLink !== params.get('categoryLink')) {
+          this.categoryLink = params.get('categoryLink');
+          this.loadCategory();
+        }
+
+        if (this.pictureLink !== params.get('pictureLink')) {
+          this.pictureLink = params.get('pictureLink');
+          this.loadPicture()
+        }
 
         console.log(this.categoryLink, this.pictureLink);
-        this.loading = true;
-        this.category = null;
-        this.categoryService.getByLink(this.categoryLink)
-          .pipe(finalize(() => this.loading = false)).subscribe(
-          category => {
-            this.category = category;
-            this.pictureLength = this.category.pictures.length;
-            this.changeRoutePicture(0);
-          }
-        );
       }
     )
 
   }
 
-  select(picture: Picture) {
-    console.log('selected picture');
-    if (this.picture1) {
-      this.picture2 = picture;
-      this.picture1 = null;
-    } else  {
-      this.picture1 = picture;
-      this.picture2 = null;
-    }
-  }
-  onSwipe(event: SwipeGestureEventData) {
-    console.log(event);
-
-    if (event.direction === SwipeDirection.left) {
-      this.prevPicture();
-    }
-    if (event.direction === SwipeDirection.right) {
-      this.nextPicture();
-    }
-  }
-
   ngOnInit() {
-    console.log('selected picture');
+  }
+
+  scrollToIndex(index: number): void {
+    if (!this.thumbsRef.length) {
+      return;
+    }
+    let scrollView = <ScrollView>this.scrollview.nativeElement;
+
+    const firstPictureRef = <FlexboxLayout>this.thumbsRef.first.nativeElement;
+    const pictureRef = <FlexboxLayout>this.thumbsRef
+      .find((el, currentIndex) => index === currentIndex ).nativeElement;
+
+    scrollView.scrollToHorizontalOffset(
+      pictureRef.getLocationRelativeTo(firstPictureRef).x,
+      true
+    );
+  }
+
+  private loadCategory() {
+    this.category = null;
+    this.loading = true;
+
+    this.categoryService.getByLink(this.categoryLink)
+      .pipe(finalize(() => this.loading = false)).subscribe(
+      category => {
+        this.category = category;
+        this.pictureLength = this.category.pictures.length;
+        this.changeRoutePicture(0);
+      }
+    );
+  }
+
+  loadPicture() {
+    if (!this.category) {
+      return;
+    }
+
+    const index = _.findIndex(this.category.pictures, {link: this.pictureLink});
+    if (!index) {
+      return;
+    }
+
+    this.picture = this.category.pictures[index];
+    if (this.oldIndex !== null) {
+      this.oldIndex = this.index;
+    }
+    this.index = index;
+
+    if (!index) {
+      this.routerExtensions.navigateByUrl(this.category.routerLink);
+    } else {
+      this.changePicture();
+    }
+  }
+
+  select(picture: Picture) {
+    const index = _.findIndex(this.category.pictures, {id: picture.id});
+
+    this.changeRoutePicture(index);
+  }
+
+
+  changeRoutePicture(index) {
+    return this.routerExtensions.navigateByUrl(
+      this.category.pictures[index].getRouterLink(this.category)
+    );
+  }
+
+  selectedIndexChange($event) {
+    if (this.index === $event.value) {
+      return;
+    }
+
+    this.changeRoutePicture($event.value)
+      .then(() => this.scrollToIndex(this.index));
+  }
+
+
+  changePicture() {
+    this.oldIndex = this.index;
   }
 
 
@@ -95,12 +165,5 @@ export class PictureGalleryPageComponent implements OnInit {
       this.changeRoutePicture(this.index - 1);
     }
   }
-
-
-  changeRoutePicture(index) {
-    console.log(index);
-    this.select(this.category.pictures[index]);
-  }
-
 
 }
